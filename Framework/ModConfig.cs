f9704bcd2e228bcd9549fce71d4394172eb1118e
ArkejnKano <kanoakorai@gmail.com>
2021-12-26 17:44:20 -0500

    Fuck yeah basic shit

---
+++ Framework/ModConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarolineMarriage.Framework
+{
+    internal class ModConfig
+    {using System.Diagnostics.CodeAnalysis;
+
+    namespace CarolineMarriage.Framework
+    {
+        /// <summary>The mod configuration model.</summary>
+        [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Deliberately named to simplify readability for players editing the config file.")]
+        internal class ModConfig
+        {
+        }
+    }
+}
+}
---
+++ ModEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CarolineMarriage.Framework.CarolineMarriage.Framework;
+using Shared.Shared;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace CarolineMarriage
+{
+    internal class ModEntry : Mod
+    {/// <summary>The mod entry point, called after the mod is first loaded.</summary>
+     /// <param name="helper">Provides simplified APIs for writing mods.</param>
+     /// <summary>The mod instance.</summary>
+     public static ModEntry Instance { get; private set; }
+        public ModConfig Config { get; private set; }
+
+        public override void Entry(IModHelper helper)
+        {
+            IModEvents events = helper.Events;
+            Instance = this;
+            Log.Monitor = this.Monitor;
+            Config = helper.ReadConfig<ModConfig>();
+
+            HarmonyPatcher.Apply(
+                this,
+                new TemporaryAnimatedSpritePatcher(),
+                new FarmerPatcher()
+            );
+
+            ConsoleCommandHelper.RegisterCommandsInAssembly(this);
+        }
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
